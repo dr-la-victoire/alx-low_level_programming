cffe7930905d20b8d9c787f272ee09a11f599dfe
victoire <lavictoire633@gmail.com>
2023-03-16 08:26:28 -0700

    printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: This functon prints some strings to stdout
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
