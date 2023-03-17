dfc270093b9555feefcfac31037c30b80527c3ef
victoire <lavictoire633@gmail.com>
2023-03-17 06:53:37 -0700

    printing lowercase alphabets without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * 
+ * Description: This program prints the alphabet in lowercase
+ * except q and e
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101)
+			continue;
+		if (i == 113)
+			continue;
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
