219990735ecba3dcdd00ba690b7c08c923b4586b
victoire <lavictoire633@gmail.com>
2023-03-20 23:36:13 -0700

    pushing multiple files in the c functions and nested loops project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Description: This program prints "_putchar"
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char func[20] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+		_putchar(func[i]);
+
+	_putchar('\n');
+
+
+	return (0);
+}
