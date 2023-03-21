ad4e566c6903940d59df74164a399db524daa790
victoire <lavictoire633@gmail.com>
2023-03-21 11:33:35 -0700

    function to check whether a char is upper or lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * @c: the char
+ *
+ * Return: 1 (lowercase) or 0 (otherwise)
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 || c <= 122)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/3-islower_main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - checks code
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int r;
+
+	 r = _islower('H');
+	 _putchar(r + '0');
+	 r = _islower('o');
+   	 _putchar(r + '0');
+
+	 return (0);
+}
