a98478a05bd1cc90a7caa46aebea42d1480e8fda
victoire <lavictoire633@gmail.com>
2023-03-21 14:01:54 -0700

    function that prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting from 0
+ *
+ * Return: Nothing
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j != 9 && (i * j <= 9))
+			{
+				k = i * j;
+				_putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (j != 9 && (i * j >= 9))
+			{
+				k = i * j;
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (j == 9)
+			{
+				k = i * j;
+				if (k <= 9)
+					_putchar(k + '0');
+				else
+				{
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table_main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - checks code
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
