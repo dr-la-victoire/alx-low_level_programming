b519f3ec17318b8048cbdf36bebca15ff25f6f00
victoire <lavictoire633@gmail.com>
2023-03-21 12:17:54 -0700

    correction on the c program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,18 +3,21 @@
 /**
  * main - Entry Point
  *
- * Description: This program prints "_putchar"
+ * Description: This program prints the chars _putchar to stdout
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	char func[20] = "_putchar";
-	int i;
-
-	for (i = 0; i <= 8; i++)
-		_putchar(func[i]);
-
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
 
 	return (0);
 }
