ea848de9787cea8fd92930ad399b926ac78b5ae9
victoire <lavictoire633@gmail.com>
2023-03-22 07:59:51 -0700

    a program that prints the sum of the even valued numbers in the fibonacci sequence less than 4 million

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: This program prints the sum of the even valued terms
+ * in the Fibonacci sequence
+ * Return: 0  (Success)
+ */
+
+int main(void)
+{
+	unsigned long int a, b;
+	unsigned long int next;
+	unsigned long int i;
+	unsigned long int sum = 0;
+
+	for (i = 0; i < 4000000; i++)
+	{
+		a = b;
+		b = next;
+		next = a + b;
+
+		if (next % 2 == 0)
+		{
+			sum = sum + next;
+		}
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
