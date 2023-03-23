057c05a9b64831d7628904d28bca950f6fab5b81
victoire <lavictoire633@gmail.com>
2023-03-23 07:34:33 -0700

    printing fizz buzz and fizzbuzz to replace multiples of 3, 5 and multiples of both 3 and 5 respectively

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,17 +14,20 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
+		if (i != 100)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+				printf("FizzBuzz ");
+			else if (i % 3 == 0)
+				printf("Fizz ");
+			else if (i % 5 == 0)
+				printf("Buzz ");
+			else
+				printf("%d ", i);
+		}
 		else
-			printf("%d ", i);
+			printf("Buzz\n");
 	}
-
-	putchar('\n');
 
 	return (0);
 }
