17949df07a20c15d4d0f91c2c49073b034e9f237
victoire <lavictoire633@gmail.com>
2023-03-30 14:04:58 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry Point
- *
- * Description: A special program that prints the value of the
- * third element of an array
- * Return: 0 (Success)
- */
-
 int main(void)
 {
   int n;
@@ -24,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  p[5] = 98;
+ p[5] = 98 ;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
