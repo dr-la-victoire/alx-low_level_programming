bda275611b4d36175e39d124d16d08a70c1c97dd
victoire <lavictoire633@gmail.com>
2023-03-30 14:23:02 -0700

    a function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,11 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *s = dest;
+	int i;
 
-	while (*src != '\0' && n--)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
 	}
 
 	while (*dest != '\0')
