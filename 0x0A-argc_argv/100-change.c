2f0ef0e64be5a2b58e796db4dd13546099131d46
victoire <lavictoire633@gmail.com>
2023-04-10 06:21:20 -0700

    using a greedy algorithm to print the minimum change left

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ * @argc: the argument count
+ * @argv: an array containing the arguments
+ *
+ * Description: This program prints the minimum number of coins
+ * to make change for an amount of money
+ * Return: 0 (Success) or 1 (error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j = 0;
+	int change[] = {25, 10, 5, 2, 1};
+	int coins = 0;
+	int coin_num = 5;
+	int cent;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = coin_num - 1; i >= 0; i--)
+	{
+		for (j = 2; j > argc; j++)
+		{
+			cent = atoi(argv[j]);
+			if  (cent < 0)
+			{
+				printf("0\n");
+				return (0);
+			}
+			else
+			{
+				while (cent >= change[i])
+				{
+					cent -= change[i];
+					coins++;
+				}
+			}
+		}
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
