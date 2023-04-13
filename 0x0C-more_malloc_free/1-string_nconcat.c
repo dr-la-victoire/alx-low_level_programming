a3ad876635b5332ef6d14728c7e414a49e6bddff
victoire <lavictoire633@gmail.com>
2023-04-13 12:39:35 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,37 @@
 #include "main.h"
 
 /**
- * string_nconcat -  concatenates two strings
+ * string_nconcat - concatenates two strings
  * @s1: the first string
  * @s2: the second string
  * @n: the number of bytes
  *
- * Return:  point to a newly allocated space in memory, which contains s1,
+ * Return: point to a newly allocated space in memory, which contains s1,
  * followed by the first n bytes of s2, and null terminated or NULL on failure
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len1 = 0, len2 = 0, i, j, len;
-	char *catstr;
+	char *str_concat;
+	unsigned int i, j, len1 = 0, len2 = 0;
 
-	if (s1 != NULL)
-	{
-		i = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-		while (s1[i] != '\0')
-		{
-			len1++;
-			i++;
-		}
-	}
-	if (s2 != NULL)
-	{
-		i = 0;
-
-		while (s2[i] != '\0')
-		{
-			len2++;
-			i++;
-		}
-	}
-	len = len1 + len2;
-
-	catstr = (char *)malloc(sizeof(*catstr) * len + 1);
-	if (catstr == NULL)
+	if (n >= len2)
+		n = len2;
+	str_concat = (char *)malloc(len1 + n + 1);
+	if (str_concat == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		catstr[i] = s1[i];
-	for (j = 0; j < n; j++)
-		catstr[j] = s2[j];
-	catstr[len] = '\0';
 
-	return (catstr);
+	for (i = 0; i < len1; i++)
+		str_concat[i] = s1[i];
+	for (j = 0; j < n; j++)
+		str_concat[i + j] = s2[j];
+
+	str_concat[i + j] = '\0';
+
+	return (str_concat);
 }
