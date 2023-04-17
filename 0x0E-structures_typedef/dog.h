5caff1fc7bf27266409a5e490c431100a0cf08f8
victoire <lavictoire633@gmail.com>
2023-04-17 12:53:27 -0700

    definning a struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct of a dog
+ * @name: the dog name
+ * @age: the dog age
+ * @owner; the dog owner
+ *
+ * Description: a user-defined type that shows the name, age and owner
+ * of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
