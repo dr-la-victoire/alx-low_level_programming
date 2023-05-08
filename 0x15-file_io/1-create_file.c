86ef4dfa556490f3c27e0169a6a7690279a6495e
La Victoire <doctorvictory116@gmail.com>
2023-05-08 12:49:04 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	w = write(fd, text_content, len + 1);
+	w = write(fd, text_content, len);
 	if (fd == -1 || w == -1)
 		return (-1);
 	close(fd);
