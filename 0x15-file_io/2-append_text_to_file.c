da96d2aa543375cef3db37327f4f003932045e26
la victoire <lavictoire633@gmail.com>
2023-05-19 13:56:46 -0700

    mking changes and correcting errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,20 +14,13 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (text_content)
+	if (text_content != NULL)
 	{
-		while (*text_content != '\0')
-		{
+		for (len = 0; text_content[len];)
 			len++;
-			text_content++;
-		}
-
-		w = write(fd, text_content, len);
-
-		if (w == -1)
-			return (-1);
 	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	w = write(fd, text_content, len);
 	if (fd == -1)
 		return (-1);
 
