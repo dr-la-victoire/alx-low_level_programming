ec637357d57d0e31722cbf4e27da2e91beea7a4a
la victoire <lavictoire633@gmail.com>
2023-05-19 13:51:35 -0700

    making changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	while (*text_content != '\0')
+	if (text_content != NULL)
 	{
-		len++;
-		text_content++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
