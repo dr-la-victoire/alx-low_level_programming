db86a8b5b9da9b4ea495d7dec0d50fb90b799415
la victoire <lavictoire633@gmail.com>
2023-05-19 14:02:23 -0700

    fixing errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	w = write(fd, text_content, len);
-	if (fd == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
 	close(fd);
