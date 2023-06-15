ff6c6814caed6f4b934dd6b84072205704381bee
la victoire <lavictoire633@gmail.com>
2023-06-15 06:39:55 -0700

    a function that adds a node at the beginning of a linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,11 +20,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->n = n;
 	newNode->prev = NULL;
 	temp = *head;
-	if (temp != NULL)
-	{
-		while (temp->prev != NULL)
-			temp = temp->prev;
-	}
 	newNode->next = temp;
 	if (temp != NULL)
 		temp->prev = temp;
