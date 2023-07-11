8bda7f9fca013c2c2785bb211dacb9316fd59dc7
la victoire <lavictoire633@gmail.com>
2023-07-11 13:09:28 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to a hash table
- * @key: the key, which must not be empty
- * @value: the value, which must be duplicated
+ * hash_table_set - adds an element to the hash table
  * @ht: the hash table
+ * @key: the key
+ * @value: the value
  *
- * Return: 1 (success) or 0 (failure)
+ * Return: 1 (Success) or 0 (Failure)
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -14,19 +14,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, i;
 	hash_node_t *newNode;
 
-	if (*key == '\0')
-		return (0);
-
-	/* computing the index */
-	index = key_index((const unsigned char *)key, ht->size);
-
-	/* looping to find the index to add the element */
-	for (i = index; ht->array[i]; i++)
+	index = key_index((const unsigned char *)key, ht->size); /* getting index */
+	for (i = index; ht->array[i]; i++) /* going to the point to place the data */
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0) /* checking for similarities */
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = strdup(value);
+			ht->array[i]->value = strdup(value); /* setting the value */
 			return (1);
 		}
 	}
@@ -34,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	newNode = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
-	newNode->key = strdup(key); /* in case of collusion */
+	newNode->key = strdup(key);
 	newNode->value = strdup(value);
 	newNode->next = ht->array[index];
 	ht->array[index] = newNode;
